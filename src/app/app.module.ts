03149e333acd13e4e67263559fc9de0a19e68c5e
SAIKIRANVEMUNURI <saikiranreddyvemunuri@gmail.com>
2020-05-06 22:59:06 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MyCoursesComponent } from './my-courses/my-courses.component';
+import { RegisterComponent } from './register/register.component';
+
+const appRoutes : Routes=[
+  {path:'',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'javacourse/:coursecode',component:MyCoursesComponent,data:{username:'Sai',role:'Manager'}},
+  {path:'pythoncourse/:coursecode',component:MyCoursesComponent,data:{username:'pranay',role:'Senoir Manager'}},
+  {path: '**',redirectTo:'',pathMatch:'full'}
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    LoginComponent,
+    MyCoursesComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
